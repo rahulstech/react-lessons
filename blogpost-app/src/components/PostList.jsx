6d82560d8ca4b1ea1fc3d5326a8203a0930b687a
Rahul Bagchi <rahulstech18@gmail.com>
2025-03-14 23:21:35 +0530

    styling posts

--- blogpost-app/src/components/PostList.jsx
+++ blogpost-app/src/components/PostList.jsx
@@ -3,13 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchPosts } from "../features/posts/postSlice";
 
 export default function PostList() {
-    console.log('select posts');
     const posts = useSelector(state => state.posts.posts);
-    console.log('select status');
     const status = useSelector(state => state.posts.status);
-    console.log('select error');
     const error = useSelector(state => state.posts.error);
-    console.log('get dispatcher');
     const dispatch = useDispatch();
 
     useEffect(()=> {
@@ -24,18 +20,34 @@ export default function PostList() {
         content = <p>{error.message}</p>
     }
     else {
-        content = posts.map(post => (
-            <div key={post.id}>
-                <p>{post.title}</p>
-                <p>{post.body}</p>
-            </div>
-        ))
+        content = (<div className="row mt-5 gy-2">
+            {
+                posts.map(post => (
+                    <div className="col-sm-12 col-md-6 col-lg-4" key={post.id}>
+                        <div className="card">
+                            <div className="card-body p-3">
+                                <div className="row mb-3 align-items-start">
+                                    <div className="col">
+                                        <h4 className="card-title">{post.title}</h4>
+                                    </div>
+                                    <div className="col-auto">
+                                        <a href="#" className="btn btn-primary">View Post</a>
+                                    </div>
+                                </div>
+                                <p className="card-text text-body-secondary">{post.body}</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                ))
+            }
+        </div>)
     }
     
     console.log(`rendering postlist status=${status} error=${error}`);
     return (
         <div>
-            <p>Posts</p>
+            <p className="display-5 text-center">Posts</p>
             {content}
         </div>
     );

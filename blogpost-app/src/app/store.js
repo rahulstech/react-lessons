ec0e417fca646cc5ce9f2db6e3f7277fa9f34a5c
Rahul Bagchi <rahulstech18@gmail.com>
2025-03-21 01:28:28 +0530

    ignored

--- blogpost-app/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { postReducer } from '../features/posts/postSlice';
-
-export const appStore = configureStore({
-    reducer: {
-        posts: postReducer,
-    }
-});
--- redux-toolkit/src/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { todoReducer } from './todoSlice';
-
-const todoStore = configureStore({
-    reducer: todoReducer
-});
-
-export default todoStore;
